6013d9d079688828fd462b18c3df4642cbd70c43
Abhishek Pandey <88980729+Abhishekabh99@users.noreply.github.com>
2022-08-30 18:10:14 +0530

    first commit (folder structure basic) setup

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './about.css'
+//functional component using the arrow function
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
